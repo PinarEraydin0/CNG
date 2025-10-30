f94ef31a780dac9440b7a54e72dfc08b19c0dfca
PinarEraydin0 <240201012@sivas.edu.tr>
2025-10-30 20:43:43 +0300

    Add files via upload

---
+++ t4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Temperature {
+public:
+    double celsius;
+
+    double toFahrenheit() {
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+
+    double toKelvin() {
+        return celsius + 273.15;
+    }
+};
+
+int main() {
+    Temperature temp;
+
+    cout << "Enter temperature in Celsius: ";
+    cin >> temp.celsius;
+
+    cout << "Temperature in Fahrenheit: " << temp.toFahrenheit() << " °F" << endl;
+    cout << "Temperature in Kelvin: " << temp.toKelvin() << " K" << endl;
+
+    return 0;
+}
+
