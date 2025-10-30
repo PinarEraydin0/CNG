40b3a6d66c251186bea7986f7cd7179eeb651420
PinarEraydin0 <240201012@sivas.edu.tr>
2025-10-30 20:18:28 +0300

    Add files via upload

---
+++ t3.cpp
@@ -0,0 +1,26 @@
+﻿// t3.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+    int r;
+
+    void calculateArea() {
+        const double pi = 3.14;
+        double area = pi * r * r;
+        cout << "Area: " << area << endl;
+    }
+};
+
+int main() {
+    Circle c;
+
+    cout << "Radius: ";
+    cin >> c.r;
+
+    c.calculateArea(); 
+
+    return 0;
+}
