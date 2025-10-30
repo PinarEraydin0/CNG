bfd4477aa0ce43f6952559300b73e34daa645396
PinarEraydin0 <240201012@sivas.edu.tr>
2025-10-30 13:34:18 +0300

    Add files via upload

---
+++ t2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class ArrayOperations {
+public:
+    int array[5];
+
+    void inputArray() {
+        for (int i = 0; i < 5; i++) {
+            cout << "Array " << i + 1 << ": ";
+            cin >> array[i];
+        }
+    }
+
+    void calculateSum() {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += array[i];
+        }
+        cout << "Sum: " << sum << endl;
+    }
+};
+
+int main() {
+    ArrayOperations arr;
+
+    arr.inputArray();     
+    arr.calculateSum();   
+
+    return 0;
+}
+
+
+
