76384d8d7cf85ebc63ad5d5e8fa325d1e5eb2d56
PinarEraydin0 <240201012@sivas.edu.tr>
2025-10-30 21:23:36 +0300

    Add files via upload

---
+++ t5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int grades[3];
+    double average;
+
+    void calculateAverage() {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += grades[i];
+        }
+        average = sum / 3.0; 
+    }
+};
+
+int main() {
+    Student student;
+    student.name = "Ali";
+
+    cout << "Enter 3 grades: ";
+    for (int i = 0; i < 3; i++) {
+        cin >> student.grades[i];
+    }
+
+    student.calculateAverage();
+
+    cout << "Student Name: " << student.name << endl;
+    cout << "Average Grade: " << student.average << endl;
+
+    return 0;
+}
