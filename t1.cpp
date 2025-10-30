fd000bef948f430c01164e7ca406911e3ee1e743
PinarEraydin0 <240201012@sivas.edu.tr>
2025-10-30 13:03:38 +0300

    Add files via upload

---
+++ t1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+    int width;
+    int height;
+
+    void calculateArea() {
+        int area = width * height;
+        cout << "Area: " << area << endl;
+    }
+};
+
+int main() {
+    Rectangle rect;
+
+    cout << "Enter width: ";
+    cin >> rect.width;
+
+    cout << "Enter height: ";
+    cin >> rect.height;
+
+    rect.calculateArea(); 
+
+    return 0;
+}
+
+
